f1c36dac87d2e9dbb1eba3b72c9b20194994e079
Elton Leander Pinto <eltonp3103@gmail.com>
2026-02-16 14:35:57 -0500

    Format

--- csrc/setu/node_manager/worker/NCCLWorker.h
+++ csrc/setu/node_manager/worker/NCCLWorker.h
@@ -46,7 +46,6 @@ using setu::planner::ir::llc::Instruction;
 using setu::planner::ir::llc::Program;
 using setu::planner::ir::llc::Receive;
 using setu::planner::ir::llc::Send;
-using setu::planner::ir::llc::Barrier;
 using setu::planner::ir::llc::UseComm;
 //==============================================================================
 
